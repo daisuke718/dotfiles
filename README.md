d964f2c7f81703137e28bbd03428d0302d47891e
弘光 大輔 <hiromitsu.daisuke@gmail.com>
2017-08-30 13:36:46 +0900

    Add install shell

--- README.md
+++ README.md
@@ -1,15 +1,11 @@
 # dotfiles
 
-- vimrc install memo
+- インストール手順
+    - 前提条件
+        - Homebrewインストール済であること
+    - 手順
+        ```sh
+        $ cd dotfiles
+        $ ./install.sh
+        ```
 
-   ```sh
-   $ mkdir -p ~/.vim/bundle
-   $ git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-   ```
-
-- ctags memo
-    - Mac OSX
-
-     ```sh
-     $ brew install ctags
-     ```
---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR=`dirname $0`
+cd ${CURRENT_DIR}
+dotfiles_path=`pwd`
+
+files=`find . -name "_*"`
+for file in ${files} ; do
+    filename=`basename ${file}`
+    ln -s ${dotfiles_path}/${filename} ~/${filename/_/.}
+done
+
+# Change Login Shell
+chsh -s /bin/zsh
+
+#=================#
+# vim             #
+#=================#
+# install vim bundle
+mkdir -p ~/.vim/bundle
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
+# make vim backup dir
+mkdir -p ~/backup/vim
+
+# ctags install
+brew install ctags
+
+#=================#
+# zsh             #
+#=================#
+# make zsh plugin dir
+mkdir -p ~/.zsh_plugins
+
+# install zaw
+git clone git://github.com/zsh-users/zaw.git ~/.zsh_plugins/zaw
+
+# make cdr cache dir
+mkdir -p .zshcache/shell
+
